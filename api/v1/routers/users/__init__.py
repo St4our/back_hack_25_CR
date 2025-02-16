219cfc69a4e23572cc60eab692c9f97edbb4e845
Gaveyn <crstalhopkin@gmail.com>
2025-02-16 14:47:14 +0500

    Fix bug with user_logs routers

--- api/v1/routers/users/__init__.py
+++ api/v1/routers/users/__init__.py
@@ -2,11 +2,14 @@ from fastapi import APIRouter
 
 from .remove import router as router_remove
 from .change_password import router as router_change_password
+from .create import router as router_create_log
+from .get_all_logs import router as router_get_all_logs
+from .get_log import router as router_get_log
 
 router = APIRouter(
     prefix="/users",
     tags=["Users"]
 )
 
-routers = [router_remove, router_change_password]
+routers = [router_remove, router_change_password, router_create_log, router_get_log, router_get_all_logs]
 [router.include_router(_router) for _router in routers]
