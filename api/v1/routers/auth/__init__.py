42c88c68846dce5ac916ee046a5de8a0ad7235ac
Gaveyn <crstalhopkin@gmail.com>
2025-02-16 07:00:37 +0500

    Fix bugs with routes

--- api/v1/routers/auth/__init__.py
+++ api/v1/routers/auth/__init__.py
@@ -3,11 +3,12 @@ from fastapi import APIRouter
 from .login import router as router_login
 from .register import router as router_register
 from .verify_code import router as router_verify_code
+from .auth_from_rsaag import router as router_auth_rsaag
 
 router = APIRouter(
     prefix="/auth",
     tags=["Auth"]
 )
 
-routers = [router_login, router_register, router_verify_code]
+routers = [router_login, router_register, router_verify_code, router_auth_rsaag]
 [router.include_router(_router) for _router in routers]
--- api/v1/routers/fighter/__init__.py
+++ api/v1/routers/fighter/__init__.py
@@ -2,12 +2,11 @@ from fastapi import APIRouter
 
 from .get_fighters import router as router_fighters
 from .get_fighter import router as router_fighter
-from .create import router as router_create_fighter
 
 router = APIRouter(
     prefix="/fighter",
     tags=["Fighter"]
 )
 
-routers = [router_fighter, router_fighters, router_create_fighter]
+routers = [router_fighter, router_fighters]
 [router.include_router(_router) for _router in routers]
